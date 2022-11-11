b84017ba3ac165662b94d54cd44e9b1cfa210c6e
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2022-11-11 10:58:37 +0600

    loader is add and project is completed

--- Qoute Generator/script.js
+++ Qoute Generator/script.js
@@ -3,11 +3,22 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
-
+const loader = document.getElementById("loader");
 //global variable
 let apiQuote = [];
 
+// showing loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+// hiding loader
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
 function newQuote() {
+  loading();
   //picking up one quote
   const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)];
   if (quote.text.length > 100) {
@@ -21,8 +32,10 @@ function newQuote() {
   } else {
     authorText.textContent = quote.author;
   }
+  complete();
 }
 async function getQuote() {
+  loading();
   const apiUrl = `https://type.fit/api/quotes`;
   try {
     const response = await fetch(apiUrl);
@@ -41,3 +54,6 @@ newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
 //on load
 getQuote();
+
+// loader
+// loading();
